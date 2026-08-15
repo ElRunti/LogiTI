00a1207294b84a7412ab9684a26d754c8b5dfa5c
ElRunti <ruben060805@outlook.com>
2026-08-14 20:56:12 -0600

    feat: add DriverRepository implementation

--- FleetPulse.API/obj/Debug/net8.0/FleetPulse.API.AssemblyInfo.cs
+++ FleetPulse.API/obj/Debug/net8.0/FleetPulse.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FleetPulse.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aa1e70aa4c3333aea0576da9510aaca9f85d724")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c710a09df703af7c949f15a20ebd49d7dd8522f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FleetPulse.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FleetPulse.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
