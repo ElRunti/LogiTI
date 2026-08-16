7204307fd0eecdf33387deb2ceb1c57f7f03fdf9
ElRunti <ruben060805@outlook.com>
2026-08-15 22:41:18 -0600

    feat: complete service contracts

--- FleetPulse.API/Services/Contracts/ICustomerService.cs
+++ FleetPulse.API/Services/Contracts/ICustomerService.cs
@@ -1,6 +1,13 @@
-﻿namespace FleetPulse.API.Services.Contracts
+﻿using FleetPulse.API.DTOs.Customer;
+
+namespace FleetPulse.API.Services.Contracts
 {
     public interface ICustomerService
     {
+        public Task<CustomerDto> CreateCustomerAsync(CustomerCreateDto customerCreateDto);
+        public Task<CustomerDto> UpdateCustomerAsync(int customerId, CustomerUpdateDto customerUpdateDto);
+        public Task<CustomerDto> GetCustomerByIdAsync(int customerId);
+        public Task<IEnumerable<CustomerDto>> GetAllCustomersAsync();
+        public Task<bool> DeleteCustomerAsync(int customerId);
     }
 }
--- FleetPulse.API/Services/Contracts/IPackageService.cs
+++ FleetPulse.API/Services/Contracts/IPackageService.cs
@@ -1,6 +1,13 @@
-﻿namespace FleetPulse.API.Services.Contracts
+﻿using FleetPulse.API.DTOs.Package;
+
+namespace FleetPulse.API.Services.Contracts
 {
     public interface IPackageService
     {
+        public Task<PackageDto> CreatePackageAsync(PackageCreateDto packageCreateDto);
+        public Task<PackageDto> UpdatePackageAsync(int packageId, PackageUpdateDto packageUpdateDto);
+        public Task<PackageDto> GetPackageByIdAsync(int packageId);
+        public Task<IEnumerable<PackageDto>> GetAllPackagesAsync();
+        public Task<bool> DeletePackageAsync(int packageId);
     }
 }
