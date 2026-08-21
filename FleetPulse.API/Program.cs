8a75374467c362a7e1612d131c5f341ec682a59e
ElRunti <ruben060805@outlook.com>
2026-08-21 14:55:01 -0600

    fix: enable Swagger in production

--- FleetPulse.API/Program.cs
+++ FleetPulse.API/Program.cs
@@ -45,11 +45,8 @@ builder.WebHost.UseUrls("http://0.0.0.0:8080");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 //app.UseHttpsRedirection();
 
